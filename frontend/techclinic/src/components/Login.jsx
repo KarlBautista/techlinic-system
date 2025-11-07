c225dc8ef661b049a0ecd1b9f0c4a39da5677c7f
Richmon Jay Francisco <jayjo.frncsc@gmail.com>
2025-11-07 20:34:57 +0800

    added google

--- frontend/techclinic/src/components/Login.jsx
+++ frontend/techclinic/src/components/Login.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
+import Google from"../assets/image/google.png"
+import TUP from "../assets/image/tup.png"
 import "../App.css"
 import { useState, useRef} from "react";
+import useAuth from "../store/useAuthStore"
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signInWithGoogle, authenticatedUser } = useAuth();
+
+  const handleSignInWithGoogle = async () => {
+    try{
+      const response = await signInWithGoogle();
+      if(response.error){
+        alert(`Error signing in with google: ${response.error}`);
+        return;
+      }
+      if(response.data){
+        alert("Success signing it with google");
+        console.log(authenticatedUser);
+      }
+    } catch(err){
+      console.error(err);
+    }
+  }
     
   return (
     <div className='fullScreen min-w-[640px]'>
-
     <form action="" className='form gap-5'>
+
+        <div className='logoDiv'>
+            <div className='logo'>
+              <img src={TUP} alt="TUP" className='h-full' />
+            </div>
+            <div className='rightLogo flex flex-col justify-center '>
+              <div>
+                  <p className='tracking-[10px] text-[2rem] text-[#FF3A3A] font-bold'> TechClinic</p>
+              </div>
+              <div className='text-[#A12217]'>
+                <p>Secure Electronic Medical Record</p>
+              </div>
+            </div>
+        </div>
+
+
         <div className='formDiv'>
             <input
               type="text"
@@ -28,7 +66,7 @@ const Login = () => {
             <label htmlFor="password">Password</label>
         </div>
 
-        <button className='tracking-[5px] bg-[#B22222] w-[40%] py-[10px] text-white rounded-lg sm:bg-black md:bg-blue-100'>LOGIN</button>
+        <button className='tracking-[5px] bg-[#B22222] w-[40%] py-[10px] text-white rounded-lg'>LOGIN</button>
 
         <div className='h-[30px] w-[40%]  flex items-center justify-center'>
           <div className='w-[35%] border-1 border-gray-300'></div>
@@ -36,23 +74,14 @@ const Login = () => {
           <div className='w-[35%] border-1 border-gray-300'></div>
         </div>
 
-        <div className='bg-red-100 h-[70px] w-[40%] flex items-center justify-evenly'>
-            <button className='h-[60px] w-[60px] bg-blue-100 rounded-full'>
-                <img src="" alt="Google" />
-            </button>
-            <button className='h-[60px] w-[60px] bg-blue-100 rounded-full'>
-                <img src="" alt="Google" />
-            </button>
-
-          <button className='h-[60px] w-[60px] bg-blue-100 rounded-full md:h-[40px] md:w-[40px] sm:h-[30px] sm:w-[30px]'>
-                <img src="" alt="Google" />
+        <div className= 'h-[70px] w-[40%] flex items-center justify-evenly'>
+          <button className='h-[60px] w-[60px] bg-gray-100 rounded-full'
+          onClick={() => handleSignInWithGoogle()}>
+                <img src={Google} alt="Google" />
             </button>
         </div>
 
     </form>
-
-
-
 
 
     <div className='h-full w-[40%] bg-[#62b6cb]'> 
