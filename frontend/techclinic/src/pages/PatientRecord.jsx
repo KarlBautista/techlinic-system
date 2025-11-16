6b7808a93288858f0a0416a5c3c467db60988051
Karl <karlbautista234@gmail.com>
2025-11-16 21:28:16 +0800

    changed the hover temporarily

--- frontend/techclinic/src/pages/PatientRecord.jsx
+++ frontend/techclinic/src/pages/PatientRecord.jsx
@@ -84,7 +84,7 @@ const PatientRecord = () => {
                 <div className='w-[90%] h-full overflow-y-auto '>
                  {filteredRecords?.length > 0 ? (
                     filteredRecords.map((patient) => (
-                      <div key={patient.id} className='studentCss cursor-default hover:bg-red-300' onClick={() => handleIndividualRecord(patient.id)}>
+                      <div key={patient.id} className='studentCss cursor-default hover:underline hover:decoration-[#A12217] hover:decoration-2' onClick={() => handleIndividualRecord(patient.id)}>
                         <div className='studentInfoContainer'>
                           <p className='studentInfoData'>{patient.student_id}</p>
                         </div>
