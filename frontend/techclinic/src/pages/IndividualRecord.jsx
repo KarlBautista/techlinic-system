84d83233233d263ce23375337dd492aefad4bcb1
Karl <karlnino.bautista@tup.edu.ph>
2025-11-22 19:01:58 +0800

    added sticky positioning in navbar

--- frontend/techclinic/src/pages/IndividualRecord.jsx
+++ frontend/techclinic/src/pages/IndividualRecord.jsx
@@ -54,8 +54,8 @@ const handleBack = () => {
 
 
   return (
-    <div className='flex h-full w-full gap-2 '>
-      <div className='sm:w-[30%] w-[17%] h-full md:w-[25%] lg:w-[17%] '>
+    <div className='flex h-full w-full gap-2 overflow-y-auto'>
+      <div className='sm:w-[30%] w-[17%] h-full md:w-[25%] lg:w-[17%]  sticky top-0 '>
           <Navigation />
       </div>
       <div className='w-[83%]  h-auto flex p-5  flex-col gap-5'>
--- frontend/techclinic/src/pages/PatientRecord.jsx
+++ frontend/techclinic/src/pages/PatientRecord.jsx
@@ -33,7 +33,7 @@ const PatientRecord = () => {
   
   return (
     <div className='flex h-full w-full gap-2'>
-        <div className='sm:w-[30%] w-[17%] h-full md:w-[25%] lg:w-[17%]'> 
+        <div className='sm:w-[30%] w-[17%] h-full md:w-[25%] lg:w-[17%] sticky top-0'> 
           <Navigation />
         </div>
         <div className='w-[83%]  h-full flex justify-center p-5'>
