8cd11d5f7e77c30bb63ec3632e8c5d6e7c892b8c
Richmon Jay Francisco <jayjo.frncsc@gmail.com>
2025-12-02 02:38:29 +0800

    new analytics

--- frontend/techclinic/src/pages/newMedicine.jsx
+++ frontend/techclinic/src/pages/newMedicine.jsx
@@ -45,6 +45,9 @@ const newMedicine =()=> {
             <div className='h-[8%] w-full order-last sm:order-0 sm:w-[23%] sm:h-full md:w-[19%] lg:w-[17%]'>
                 <Navigation />
             </div>
+
+        
+
             <div className='h-[92%] min-w-[360px] sm:min-w-0  w-full sm:h-full sm:w-[77%] md:w-[81%] lg:w-[83%] overflow-auto p-5'>
                 <div className='w-full h-full flex flex-col gap-4 items-center'>
                     <div className='h-[10%]  w-full flex flex-col gap-1'>
